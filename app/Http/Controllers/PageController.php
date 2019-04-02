3f2568b44db30a9bd82752f50b3fe6605e9f6397
hoangminh122 <36332084+hoangminh122@users.noreply.github.com>
2019-04-03 00:28:50 +0700

    sa

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\TheLoai;
 use App\slide;
+use App\LoaiTin;
 class PageController extends Controller
 {
     //
     public function menu(){
     	$slide=slide::all();
+     	$theloai=TheLoai::all();
      	return view('pages.trangchu',['theloai'=>$theloai,'slide'=>$slide]);
      }
     public function getdangnhap(){
